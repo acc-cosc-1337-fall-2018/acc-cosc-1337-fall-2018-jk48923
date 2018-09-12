bc35ec68b0becd82a8bf08d7dcfed3269057003d
jk48923 <42867290+jk48923@users.noreply.github.com>
2018-09-11 20:53:33 -0500

    Add files via upload

---
+++ 01-basics/basics.cpp
@@ -0,0 +1,14 @@
+#include "basics.h"
+
+int sum_numbers(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+//write code for multiply_numbers function here
+
+int multiply_numbers(int num1, int num2)
+{
+	return int num1 * int num2;
+}
+
