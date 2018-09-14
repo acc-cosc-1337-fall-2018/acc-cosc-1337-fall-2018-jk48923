70e76096aaf86ffe2c1e0f88d7f2d6e80bf02be6
jk48923 <42867290+jk48923@users.noreply.github.com>
2018-09-13 19:03:31 -0500

    Update basics.h

--- 01-basics/basics.h
+++ 01-basics/basics.h
@@ -1,7 +1,7 @@
 ﻿int sum_numbers(int num1, int num2);
 
-//write function prototype for multiply_numbers with two integer parameters
-
 int multiply_numbers(int num1, int num2);
+
+void pass_by_val_by_ref(int val, int& ref);
 
 
