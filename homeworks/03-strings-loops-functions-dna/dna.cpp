ab91cb234ae31969b967d365674fb10971eb056a
jk48923 <42867290+jk48923@users.noreply.github.com>
2018-09-19 15:31:13 -0500

    Update dna.cpp

--- homeworks/03-strings-loops-functions-dna/dna.cpp
+++ homeworks/03-strings-loops-functions-dna/dna.cpp
@@ -3,46 +3,59 @@
 #include<string>
 #include<iostream>
 
-int get_point_mutations(std::string dna1, std::string dna2)
-{	//write the function code 
+// Get dna mutations 
+//std::string dna_mutations_1
+//std::string dna_mutations_2 = 
 
-	dna1 = GAGCCTACTAACGGGAT;
-	dna1 reverse = "";
+int get_point_mutations(std::string dna_mutations_1, std::string dna_mutations_2)
+{	//write the function code 
+/*
+	int dna1 = GAGCCTACTAACGGGAT;
+	int dna1 reverse = "";
 
 	for (unsigned int i = name.length() - 1 : i != -; --1)
 	{
 		reverse += dna1[i];
 	}
-	
-	cout << reverse;
 
+	cout << reverse;
+*/
 	return 0;
 }
 
 
 //write function code for std::string get_dna_complement(std::string dna)
 
-std::string get_dna_complement(std::string dna) // My Edit
+// Get dna complement define
+std::string dna_complement_1 = "ACCGGGTTTT";
+std::string dna_complement_2 = "ATTTTCCGGG";
+
+std::string get_dna_complement(std::string dna_complement_1, std::string dna_complement)
 {
-	return dna;
+	return dna_complement_1, dna_complement_2;
 }
 
+// Get dna transcribe define
+std::string dna_transcribe_1 = "GAUGGAACUUGACUACGUAAAUU";
+std::string dna_transcribe_2 = "AAAUUGAUCUUGACUACGUGGAA";
 
 //write function code for std::string transcribe_dna_into_rna(std::string dna);
-
-std::string transcribe_dna_into_rna(std::string dna) // My Edit
+std::string transcribe_dna_into_rna(std::string dna_transcribe_1, std::string dna_transcribe_2)
 {
-	return dna;
+	return dna_transcribe_1, dna_transcribe_2;
 }
 
-double get_gc_content(std::string dna)
+// Get dna content define
+std::string dna_content = "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT";
+
+double get_gc_content(std::string dna_content)
 {
-	auto size = dna.size(); //get the size of string let auto handle the data type
+	auto size = dna_content.size(); //get the size of string let auto handle the data type
 	auto gc_count{ 0 }; //initialize to 0 (int) let auto handle data type
 
 	//for ranged loop, for each character in loop, s holds one char
 	//auto determines data type in this case s is a char data type
-	for (auto s : dna)
+	for (auto s : dna_content)
 	{
 		//in C++ logical and is && and logical or is ||
 		//usage (and) condition1 && condition2 --- usage (or) condition1 || condition2 
@@ -60,7 +73,7 @@ double get_gc_content(std::string dna)
 	//that is a result with decimal values
 	//static_cast converts a data type from one to another
 	//here temporarily convert size to double
-	auto gc_content = (gc_count / static_cast<double>(size))*100;
+	auto gc_content = (gc_count / static_cast<double>(size)) * 100;
 
 	//custom function to round to a desired precision
 	return round_to_precision(gc_content, 2);
