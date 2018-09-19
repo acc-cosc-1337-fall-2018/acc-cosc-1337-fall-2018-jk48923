9e193686e61ad004920d12de17298204a5079ec7
jk48923 <42867290+jk48923@users.noreply.github.com>
2018-09-19 15:29:41 -0500

    Update main.cpp

--- homeworks/03-strings-loops-functions-dna/main.cpp
+++ homeworks/03-strings-loops-functions-dna/main.cpp
@@ -1,20 +1,28 @@
 //write the dna.h include statement here
 #include "dna.h"
 
+using std::string; using std::cout;
+
 int main()
 {
 	//call the dna functions from dna.h and display output
 	//Use the function arguments from the test cases in dna_test.cpp
 	//Example:
-	
+
+
 	auto point_mutation = get_point_mutations("GAGCCTACTAACGGGAT", "CATCGTAATGACGG");
-	//std::cout << point_mutation;
+	cout << point_mutation;
+
 
 	auto point_complement = get_dna_complement("AAAACCCGGT", "ACCGGGTTTT");
-	//std::cout << point_complement;
+	cout << point_complement;
 
-	auto point_transrcribe = transcribe_dna_into_rna("GATGGAACTTGACTACGTAAATT", "GAUGGAACUUGACUACGUAAAUU");
-	//std::cout << point_transrcribe;
+	//auto point_transrcribe = transcribe_dna_into_rna("GATGGAACTTGACTACGTAAATT", "GAUGGAACUUGACUACGUAAAUU");
+	auto point_transrcribe = transcribe_dna_into_rna("GATGGAACTTGACTACGTAAATT");
+	cout << point_transrcribe;
+
+	auto point_get = get_gc_content("CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT");
+	cout << point_get;
 
 	return 0;
 }
