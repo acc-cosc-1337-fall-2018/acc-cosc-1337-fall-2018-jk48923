83f83b743120e4fc1d60b6854cf455368b5c5574
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-09-28 18:37:24 -0500

    statement to display extended cost to screen?

--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -1,9 +1,11 @@
 #include "invoice_detail.h"
+#include<iostream>
 
 int main()
 {
 
 	InvoiceDetail InvoiceDetail_1(10, 100);
+	std::cout<<InvoiceDetail_1.get_extended_cost();//output to screen
 
 	return 0;
 }
