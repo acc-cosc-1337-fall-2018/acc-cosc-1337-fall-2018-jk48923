4ad4cb30f6d3b5582742d6225801b4b4c892d1b8
jk48923 <42867290+jk48923@users.noreply.github.com>
2018-09-29 16:53:35 -0500

    Update main.cpp
    fixed statement to display extended cost to screen

--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -1,9 +1,12 @@
 #include "invoice_detail.h"
+#include<iostream>
 
 int main()
 {
 
 	InvoiceDetail InvoiceDetail_1(10, 100);
+	std::cout<<InvoiceDetail_1.get_extended_cost(); //output to screen
 
 	return 0;
+	
 }
