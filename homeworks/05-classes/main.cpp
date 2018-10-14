f194b5d045e1015d8ef1bba24322ceae2a491296
jk48923 <42867290+jk48923@users.noreply.github.com>
2018-10-13 21:20:46 -0500

    Update main.cpp
    Moved user choice code to main from ostream
    Created working for loop and while loop for b.game_over
    Linked istream position choice from main to istrean function with b calling
    Created ostream menu board linked to pegs vector
    Edited check_board_full iteration and removed cout
    Add if control statement to check if position entry is between 1 and 9
    Other tweaks

--- homeworks/05-classes/main.cpp
+++ homeworks/05-classes/main.cpp
@@ -5,20 +5,6 @@
 using std::cout; using std::cin; 
 using std::endl; using std::vector;
 
-//1) Create a program that will play the TicTacToe game until the user opts to quit(outer loop).
-//2) Start the game with X or O.
-//3) Create another loop(inner loop) that iterates until a winner is determined
-//4) In the loop, prompt the user for a position from 1 through 9 and update board position.(Assume user never chooses a taken position)
-//5) Display the current status of the board.
-//6) After a winner is determined prompt user if they want to play another game.
-
-
-//Create 3 instances of TicTacToeBoard a, b, and c.
-//Play 3 games where x, o, and c win 1 game each.
-//Use the overloaded >> operator to capture positions from keyboard
-
-
-
 int main()
 {
 
@@ -29,25 +15,49 @@ int main()
 
 	vector<TIC_TAC_TOE_BOARD> types{ a, b, c };
 
-	// Iterate the vector of TicTacToeBoard
-	// sum the winners using the statement result += current_board_in_vector
-	
-	for (auto i : types)
+	string choice;
+
+	while (true)
 	{
-		cout << i.get_player() << endl;
-		//result += i;
+
+		cout << "Play Tic Tac Toe?" << endl;
+		cout << endl;
+		cout << "Choice x = Player X" << endl;
+		cout << "Choice o = Player O" << endl;
+		cout << endl;
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		if (choice == "X" || choice == "O" || choice == "x" || choice == "o")
+		{
+
+			for (auto b : types)
+			{
+				while (b.game_over() == false)
+				{
+					//b.start_game("X");
+					//b.start_game("O");
+
+					cin >> b;
+
+					result = result + b;
+
+					cout << b << endl;
+				}
+
+			}
+
+			cout << endl;
+			cout << "Player " << b.get_player() << " is the winner!" << endl;
+			cout << endl;
+		}
+
+		else
+			cout << "Please enter X or O!";
+
+		return false;
+	
 	}
-
-	//result += b i
-	//for( b : bords) - boards maybe types
-	//	while(b.game_over() = )
-	//		cin >> b
-	//cout result;
-
-
-	std::cout << a << std::endl;
-	//::cout << b << std::endl;
-	//std::cout << c << std::endl;
 
 	return 0;
 }
